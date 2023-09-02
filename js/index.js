fae54477168a5edd2638d1c27f5e1341783d6225
Shariar <shariarnafis86@gmail.com>
2023-09-02 22:20:44 +0600

    third commit

--- js/index.js
+++ js/index.js
@@ -1,28 +1,96 @@
-const loadPhTube = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000')
-    const data = await res.json();
-    const cards = data.data;
-    displaycards(cards)
+
+
+
+const loadCategory = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+  const data = await res.json()
+  const btns = data.data;
+  console.log(btns)
+  displayBtns(btns);
 }
+
+const displayBtns = btns => {
+  const btnContainer = document.getElementById('btn-container');
+  btns.forEach(btn => {
+    const categoriesBtn = document.createElement('div')
+    categoriesBtn.classList = `text-center flex-row`;
+    categoriesBtn.innerHTML = `<button class="text-white text-xs font-medium bg-[#FF1F3D] py-[10px] px-[10px] lg:px-[20px] rounded-md" onClick=loadPhTube(${btn.category_id})>${btn.category}</button>`
+    btnContainer.appendChild(categoriesBtn)
+  })
+}
+
+loadCategory()
+
+const defaultTube = async () => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+  const data = await res.json();
+  const cards = data.data;
+  console.log(cards)
+  displaycards(cards)
+}
+
+const loadPhTube = async (categoryId) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+  const data = await res.json();
+  const cards = data.data;
+  console.log(cards)
+  displaycards(cards)
+}
+
+function timeConvert(seconds) {
+  seconds = Number(seconds);
+  let hour = Math.floor(seconds / 3600);
+  let minute = Math.floor(seconds % 3600 / 60);
+  let second = Math.floor(seconds % 3600 % 60);
+
+  let hourDisplay = hour > 0 ? hour + (hour == 1 ? " hour, " : " hours, ") : "";
+  let minuteDisplay = minute > 0 ? minute + (minute == 1 ? " minute, " : " minutes, ") : "";
+  return(hourDisplay + minuteDisplay)
+}
+
 
 const displaycards = cards => {
     const cardContainer = document.getElementById('card-container')
-    cards.forEach(card => {
-        console.log(card.title)
+    cardContainer.innerHTML = "";
+    const errorContainer = document.getElementById('errorContainer')
+    errorContainer.innerHTML = "";
+    const errorMsg = document.createElement('div');
+    
+    {
+      cards.length ?
+      cards.forEach(card => {
         const tubeCard = document.createElement('div')
-        tubeCard.classList = `card w-96 bg-base-100 shadow-xl  w-[312px]`
-        tubeCard.innerHTML = 
-        `<figure><img src="${card.thumbnail}" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">${card.title}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+        tubeCard.classList = `card mx-auto p-3 bg-base-100 shadow-xl w-[312px]`
+        tubeCard.innerHTML =
+          `<figure><img class="relative" src="${card.thumbnail}" alt="" /> 
+            <span class="absolute right-6 text-xs top-36 text-white font-semibold bg-gray-900 p-1 rounded">${timeConvert(card?.others?.posted_date)}</span> 
+          </figure>
+          <div class="mt-5 gap-3 flex">
+            <div class="w-8">
+            <img class="rounded-full" src="${card.authors[0].profile_picture}" alt="" />
+            </div>
+            <div>
+            <h2 class="card-title text-black">${card.title}</h2>
+            <h2 class=" text-gray-500 text-xs">${card.authors[0].profile_name}
+            <span>${card?.authors[0].verified ? `<i class="fa-solid fa-circle-check"></i>` : " " }</span></h2>
+            <h2 class="text-gray-500 text-xs">${card.others.views}</h2>
+            </div>
           </div>
-        </div>
-      </div>`
-      cardContainer.appendChild(tubeCard)
-    })
-}
+        </div>`
+  
+        cardContainer.appendChild(tubeCard)
+      })
+      :
+        
+        errorMsg.innerHTML =
+          `
+          <figure><img class="mx-auto mt-24 justify-center items-center" src="Icon.png" alt="icon"/></figure>
+          <p class="text-center font-bold text-3xl">Oops!! Sorry, There is no content here</p>
+          `
+        }
+        errorContainer.appendChild(errorMsg)
+  }
 
-loadPhTube()
+
+defaultTube();
+module.export = app;
